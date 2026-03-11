9f8812c23162c005848e9dd5b568c49d52143d22
luoyi <luoyi58624@gmail.com>
2026-03-11 12:13:39 +0800

    feat: 添加 confirm 提示

--- packages/el_cli/lib/src/prompt/confirm.dart
+++ packages/el_cli/lib/src/prompt/confirm.dart
@@ -1,22 +1,25 @@
-// import 'dart:io';
-//
-// import 'package:dart_console/dart_console.dart';
-// import 'package:el_cli/el_cli.dart';
-// import 'package:io/ansi.dart';
-//
-// import 'prompt.dart';
-//
-// class Confirm extends Input {
-//   Confirm(super.message, {super.defaultValue = false}) : assert(defaultValue != null);
-//
-//   @override
-//   String get message => '${super.message} ${defaultValue! ? '(Y/n)' : '(y/N)'} ';
-//
-//   @override
-//   bool build() {
-//     final v = result;
-//     if (v == 'y' || v == 'Y' || v.toLowerCase() == 'yes') return true;
-//     if (v == 'n' || v == 'N' || v.toLowerCase() == 'no') return false;
-//     return defaultValue!;
-//   }
-// }
+import 'dart:io';
+
+import 'package:dart_console/dart_console.dart';
+import 'package:el_cli/el_cli.dart';
+import 'package:io/ansi.dart';
+
+import 'prompt.dart';
+
+class Confirm extends Prompt {
+  Confirm(super.message, {super.defaultValue = false}) : assert(defaultValue != null);
+
+  @override
+  bool get defaultValue => super.defaultValue as bool;
+
+  @override
+  String get message => '${super.message} ${defaultValue ? '(Y/n)' : '(y/N)'} ';
+
+  @override
+  bool build() {
+    final v = result;
+    if (v == 'y' || v == 'Y' || v.toLowerCase() == 'yes') return true;
+    if (v == 'n' || v == 'N' || v.toLowerCase() == 'no') return false;
+    return defaultValue;
+  }
+}
