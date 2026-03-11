2dc0464490b595fa0280aa97986dc45dab8b91e9
luoyi <luoyi58624@gmail.com>
2026-03-11 12:16:33 +0800

    feat: 添加 dart test 库

---
+++ packages/luoyi_dart_test/bin/luoyi_dart_test.dart
@@ -0,0 +1,5 @@
+import 'package:luoyi_dart_test/luoyi_dart_test.dart' as luoyi_dart_test;
+
+void main(List<String> arguments) {
+  print('Hello world: ${luoyi_dart_test.calculate()}!');
+}
---
+++ packages/luoyi_dart_test/test/luoyi_dart_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:luoyi_dart_test/luoyi_dart_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
